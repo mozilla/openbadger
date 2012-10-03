355a3b072e23eba7c68ee2a1b11e66d6fbb06e13
Brian J Brennan <brianloveswords@gmail.com>
2012-10-03 14:47:36 -0400

    Remove uncessary configuration.

--- app.js
+++ app.js
@@ -13,8 +13,6 @@ var app = express();
 
 app.configure(function() {
   app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
