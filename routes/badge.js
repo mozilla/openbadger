b0786114d227d35346b3acb9aea76d828dda263a
Brian J Brennan <brianloveswords@gmail.com>
2012-10-09 17:43:18 -0400

    CRUD skeleton.

---
+++ routes/badge.js
@@ -0,0 +1,6 @@
+var Badge = require('../models/badge');
+
+exports.create = function create(req, res) {};
+exports.read = function read(req, res) {};
+exports.update = function update(req, res) {};
+exports.destroy = function destroy(req, res) {};
