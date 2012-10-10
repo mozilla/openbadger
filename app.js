a1930f2994a3c4c16671b70ecb08dd0fc3fa15d2
Brian J Brennan <brianloveswords@gmail.com>
2012-10-09 23:58:40 -0400

    Comments for app.js

--- app.js
+++ app.js
@@ -40,43 +40,43 @@ app.configure('development', function () {
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
-app.get('/admin', admin.index);
+// getting a badge
 app.get('/admin/badge', admin.newBadgeForm);
+
+// creating a badge from post
 app.post('/admin/badge', badge.create);
 
-app.get('/admin/badges', [
-  badge.findAll
-], admin.badgeIndex);
+// show the badge index
+app.get('/', [badge.findAll], admin.badgeIndex);
+app.get('/admin', [badge.findAll], admin.badgeIndex);
+app.get('/admin/badges', [badge.findAll], admin.badgeIndex);
 
-
-var badgeByShortname = badge.findByShortname({
+// middleware for finding badge by shortname
+app.all('/admin/badge/:shortname*', badge.findByShortname({
   container: 'param',
   field: 'shortname',
   required: true
-});
+}));
 
-app.all('/admin/badge/:shortname*', badgeByShortname);
+// show edit form for single badge
 app.get('/admin/badge/:shortname', [
   behavior.findAll
 ], admin.show);
 
+// add a behavior to a badge by post
 app.post('/admin/badge/:shortname/behavior', badge.addBehavior);
+
+// remove a behavior from a badge by delete
 app.delete('/admin/badge/:shortname/behavior', badge.removeBehavior);
 
-app.get('/admin/behavior', [
-  badge.findByShortname({
-    container: 'query',
-    field: 'for',
-  })
-], admin.newBehaviorForm);
+// show new behavior form
+app.get('/admin/behavior', admin.newBehaviorForm);
 
+// create a new behavior
 app.post('/admin/behavior', behavior.create);
-
 app.get('/login', user.login);
 app.post('/login', user.login);
 app.get('/logout', user.logout);
-
 app.get('/behaviors', behavior.readAll);
 app.all('/behavior/:name', behavior.findByName);
 app.get('/behavior/:name', behavior.readOne);
