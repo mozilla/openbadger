4c1fda2601da49c0fc41f87a9106e0809097e2da
Brian J Brennan <brianloveswords@gmail.com>
2012-10-09 23:24:43 -0400

    Add shortname to behavior model.

--- models/behavior.js
+++ models/behavior.js
@@ -3,9 +3,31 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var BehaviorSchema = new Schema({
-  name: String,
-  description: String
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true
+  },
+  shortname: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true
+  }
 });
+
+function setShortnameDefault(next) {
+  if (!this.shortname && this.name)
+    this.shortname = util.slugify(this.name);
+  next();
+}
+BehaviorSchema.pre('validate', setShortnameDefault);
 
 var Behavior = db.model('Behavior', BehaviorSchema);
 module.exports = Behavior;
