440013ae840dce73604aa336901f576615291834
Brian J Brennan <brianloveswords@gmail.com>
2012-10-10 00:21:07 -0400

    Modify layout.

---
+++ public/js/login.js
@@ -0,0 +1,19 @@
+(function loginHandler () {
+  var $form = $('.js-persona-form');
+  var $input = $('.js-persona-input');
+  var $loginLink = $('.js-persona-login');
+
+  function launchBrowserId(callback) {
+    return function() {
+      navigator.id.get(callback, { siteName: 'OpenBadger' });
+    }
+  }
+
+  function handleResponse(assertion) {
+    if (!assertion) return false;
+    $input.val(assertion);
+    $form.trigger('submit');
+  }
+
+  $loginLink.bind('click', launchBrowserId(handleResponse));
+})();
