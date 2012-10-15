b9a8593a5ac4b32526dcdd8d3aaac9688ab6d58b
Brian J Brennan <brianloveswords@gmail.com>
2012-10-15 14:15:39 -0400

    Filter for tuning `undefined` into ''.

--- template.js
+++ template.js
@@ -2,4 +2,7 @@ var nunjucks = require('nunjucks');
 var env = (new nunjucks.Environment(
   new nunjucks.FileSystemLoader('views')
 ));
+env.addFilter('undef', function (thing) {
+  return thing || '';
+});
 module.exports = env;
