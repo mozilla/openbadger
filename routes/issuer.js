94d54a5cef807da5cc88a175ea18c54d63ee8427
Brian J Brennan <brianloveswords@gmail.com>
2012-10-15 14:18:01 -0400

    Route for updating config

--- routes/issuer.js
+++ routes/issuer.js
@@ -1,5 +1,22 @@
 var Issuer = require('../models/issuer');
 
+exports.update = function update(req, res) {
+  var form = req.body;
+  var issuer = req.issuer || new Issuer();
+
+  Object.keys(form).forEach(function (key) {
+    issuer[key] = form[key];
+  });
+
+  issuer.save(function (err, result) {
+    if (err)
+      return res.send(err);
+    req.flash('info', 'Configuration saved');
+    res.redirect('/');
+  });
+};
+
+
 /**
  * (Middleware) Get issuer configuration.
  */
