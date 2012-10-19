7b40f12c38c4519bf4ba06ec833673a848986cf2
Brian J Brennan <brianloveswords@gmail.com>
2012-10-19 13:35:28 -0400

    Refactor issuer model.
    * Less magic numbering
    * Use a pre-validate middleware method for setting `jwtSecret` default
      in addition to the instantiation default.
    * moar docs

--- models/issuer.js
+++ models/issuer.js
@@ -4,6 +4,15 @@ var env = require('../lib/environment');
 var util = require('../lib/util');
 var Schema = mongoose.Schema;
 
+const DEFAULT_SECRET_LENGTH = 64;
+const NAME_MAX_LENGTH = 128;
+const ORG_MAX_LENGTH = 128;
+
+
+function generateRandomSecret() {
+  return util.strongRandomString(DEFAULT_SECRET_LENGTH);
+}
+
 function maxLength(field, length) {
   function lengthValidator() {
     if (!this[field]) return true;
@@ -22,13 +31,13 @@ var IssuerSchema = new Schema({
     type: String,
     trim: true,
     required: true,
-    validate: maxLength('name', 128)
+    validate: maxLength('name', NAME_MAX_LENGTH)
   },
   org: {
     type: String,
     trim: true,
     required: false,
-    validate: maxLength('org', 128)
+    validate: maxLength('org', ORG_MAX_LENGTH)
   },
   contact: {
     type: String,
@@ -40,10 +49,28 @@ var IssuerSchema = new Schema({
     type: String,
     trim: true,
     required: true,
-    default: util.strongRandomString.bind(util, 128)
+    default: generateRandomSecret
   },
 });
 var Issuer = db.model('Issuer', IssuerSchema);
+
+/**
+ * Set a new random secret if one is not already defined.
+ * While we already have a `default` set in the schema, that only gets
+ * set at instantiation. We want to be able to delete the secret that's
+ * already there and get a new one generated for us.
+ */
+
+IssuerSchema.pre('validate', function defaultSecret(next) {
+  if (this.jwtSecret) return next();
+  this.jwtSecret = generateRandomSecret();
+  return next();
+});
+
+/**
+ * Get an object compatible with the `badge.issuer` portion of the
+ * OpenBadges spec.
+ */
 
 Issuer.getAssertionObject = function getAssertionObject(callback) {
   Issuer.findOne(function (err, issuer) {
