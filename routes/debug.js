45507b79754ca953b2017bb3b71f86cb379365ac
Brian J Brennan <brianloveswords@gmail.com>
2012-10-31 15:32:53 -0400

    Move functions out of route endpoint.

--- routes/debug.js
+++ routes/debug.js
@@ -8,22 +8,23 @@ exports.showFlushDbForm = function (req, res) {
   });
 };
 
+function removeItem(item, callback) {
+  return item.remove(callback);
+}
+function removeAfterFind(callback) {
+  return function (err, items) {
+    if (err) return callback(err);
+    return async.map(items, removeItem, callback);
+  }
+}
+function flushUsers(callback) {
+  User.find(removeAfterFind(callback));
+}
+function flushInstances(callback) {
+  BadgeInstance.find(removeAfterFind(callback));
+}
+
 exports.flushDb = function flushDb(req, res) {
-  function removeItem(item, callback) {
-    return item.remove(callback);
-  }
-  function removeAfterFind(callback) {
-    return function (err, items) {
-      if (err) return callback(err);
-      return async.map(items, removeItem, callback);
-    }
-  }
-  function flushUsers(callback) {
-    User.find(removeAfterFind(callback));
-  }
-  function flushInstances(callback) {
-    BadgeInstance.find(removeAfterFind(callback));
-  }
   async.parallel([ flushUsers, flushInstances ], function (err, results) {
     console.dir(err);
     console.dir(results);
