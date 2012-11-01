e9eb6b3bd614996d6b94b37d180f7c9f43274594
Simon Wex <simon@simonwex.com>
2012-10-31 18:28:16 -0700

    Adding auth for mongodb connections.

--- models/index.js
+++ models/index.js
@@ -2,5 +2,15 @@ var env = require('../lib/environment');
 var mongoose = require('mongoose');
 var opts = env.get('mongo');
 
-var connection = mongoose.createConnection(opts.host, opts.db, opts.port);
+var authOpts = {};
+
+if (opts.pass){
+  authOpts.pass = opts.pass;
+}
+
+if (opts.user){
+  authOpts.user = opts.user;
+}
+var connection = mongoose.createConnection(opts.host, opts.db, opts.port, authOpts);
+
 module.exports = connection;
