c1b7f94280495ebd35aca899672477aac96a8e8b
Brian J Brennan <brianloveswords@gmail.com>
2012-11-03 16:00:18 -0400

    Style; debugging.

--- public/js/badge-index.js
+++ public/js/badge-index.js
@@ -1,8 +1,10 @@
 (function ($) {
+  console.log('loaded');
   var $forms = $('.js-remove-behavior');
   var $links = $forms.find('.js-show-remove');
 
   $links.on('click', function (e) {
+    console.log('clicked');
     var $this = $(this);
     $this.siblings('.js-remove-button').show();
     $this.hide();
