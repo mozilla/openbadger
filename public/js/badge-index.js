dcf671b2be7de68729d41f16304f83b29c119b6d
Brian J Brennan <brianloveswords@gmail.com>
2012-11-03 16:34:01 -0400

    Add backout option when removing.

--- public/js/badge-index.js
+++ public/js/badge-index.js
@@ -1,13 +1,17 @@
 (function ($) {
-  console.log('loaded');
   var $forms = $('.js-remove-behavior, .js-remove-badge');
   var $links = $forms.find('.js-show-remove');
+  var $cancel = $forms.find('.js-cancel-button');
 
   $links.on('click', function (e) {
-    console.log('clicked');
     var $this = $(this);
-    $this.siblings('.js-remove-button').show();
-    $this.hide();
+    $this.parent('form').addClass('show-actions');
+    return (e.preventDefault(), false);
+  });
+
+  $cancel.on('click', function (e) {
+    var $this = $(this);
+    $this.parent('form').removeClass('show-actions');
     return (e.preventDefault(), false);
   });
 
