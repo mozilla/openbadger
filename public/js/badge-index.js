7edcda50b20d3f9e03a4a06ae9a35c0d106e050d
Brian J Brennan <brianloveswords@gmail.com>
2012-11-06 16:40:51 -0500

    Redo main page a bit.

--- public/js/badge-index.js
+++ public/js/badge-index.js
@@ -1,17 +1,26 @@
 (function ($) {
-  var $forms = $('.js-remove-behavior, .js-remove-badge');
+  var $forms = $('.js-remove-behavior');
   var $links = $forms.find('.js-show-remove');
   var $cancel = $forms.find('.js-cancel-button');
+  var $badgeDelete = $('.js-delete-badge')
 
   $links.on('click', function (e) {
     var $this = $(this);
-    $this.parent('form').addClass('show-actions');
+    $this.parents('form').addClass('show-actions');
     return (e.preventDefault(), false);
   });
 
   $cancel.on('click', function (e) {
     var $this = $(this);
-    $this.parent('form').removeClass('show-actions');
+    $this.parents('form').removeClass('show-actions');
+    return (e.preventDefault(), false);
+  });
+
+  $badgeDelete.on('click', function (e) {
+    var $this = $(this);
+    var seriously = window.confirm('Seriously? This can\'t be undone');
+    if (seriously)
+      $this.parents('form').trigger('submit');
     return (e.preventDefault(), false);
   });
 
