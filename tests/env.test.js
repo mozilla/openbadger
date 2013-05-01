84777cf29ed5db3d25b0056c6a808d4b02b7884c
Brian J Brennan <brianloveswords@gmail.com>
2013-05-01 15:27:48 -0400

    Environment method for confirming admin users

--- tests/env.test.js
+++ tests/env.test.js
@@ -2,7 +2,8 @@ var test = require('./');
 var env = require('../lib/environment');
 
 env.temp({
-  origin: 'https://example.org:443'
+  origin: 'https://example.org:443',
+  admins: ['*@example.org', 'brian@mozillafoundation.org'],
 }, function (reset) {
 
   test('env.qualifyUrl', function (t) {
@@ -16,6 +17,14 @@ env.temp({
     var expect = 'https://example.org:443';
     var result = env.origin();;
     t.same(result, expect);
+    t.end();
+  });
+
+  test('env.isAdmin', function (t) {
+    t.same(env.isAdmin('brian@example.org'), true);
+    t.same(env.isAdmin('anyone@example.org'), true);
+    t.same(env.isAdmin('guy@bad-domain.org'), false);
+    t.same(env.isAdmin('brian@mozillafoundation.org'), true);
     t.end();
   });
 });
