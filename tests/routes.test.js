e4aef3299eeb7ae5180a3756755fbbc7f411ed98
Atul Varma <varmaa@gmail.com>
2013-05-29 12:48:18 -0400

    Refactored tests/routes.test.js to make it easier to read.
    This, and its previous commits, resolves #147.

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -12,33 +12,33 @@ const api = require('../routes/api');
 var fixtures = require('./badge-model.fixtures.js');
 
 test.applyFixtures(fixtures, function () {
+  var assertionUrl, badgeUrl, criteriaUrl, issuerUrl;
+  var app = express();
+  var srv = http.createServer(app);
+
   api.jwtSecret = 'lol';
+  app.use(express.bodyParser());
+  app.use(function(req, res, next) {
+    req.session = {};
+    res.render = function(template, context) {
+      return res.send({
+        type: 'fakeRender',
+        template: template,
+        context: context
+      });
+    };
+    next();
+  });
 
-  test('claiming a badge & getting its assertion works', function(t) {
-    var app = express();
-    var srv = http.createServer(app);
+  routes.define(app);
 
-    app.use(express.bodyParser());
-    app.use(function(req, res, next) {
-      req.session = {};
-      res.render = function(template, context) {
-        return res.send({
-          type: 'fakeRender',
-          template: template,
-          context: context
-        });
-      };
-      next();
-    });
+  app.use(function(err, req, res, next) {
+    console.error("500 Internal Server Error traceback:");
+    console.error(err.stack);
+    return res.send(500, "Error, see stderr output.");
+  });
 
-    routes.define(app);
-
-    app.use(function(err, req, res, next) {
-      console.error("500 Internal Server Error traceback:");
-      console.error(err.stack);
-      return res.send(500, "Error, see stderr output.");
-    });
-
+  test('claiming a badge works', function(t) {
     request(srv)
       .post('/v2/claim')
       .send({
@@ -49,61 +49,71 @@ test.applyFixtures(fixtures, function () {
       .expect(200, function(err, res) {
         if (err) throw err;
         t.equal(res.body.status, 'ok');
-        var assertionUrl = res.body.url;
+        assertionUrl = res.body.url;
+        t.end();
+      });
+  });
 
-        request(srv)
-          .get(assertionUrl)
-          .expect(200, function(err, res) {
-            if (err) throw err;
-            t.equal(res.body.recipient.type, 'email');
-            t.equal(res.body.recipient.hashed, true);
-            t.same(res.body.verify, {
-              type: 'hosted',
-              url: assertionUrl
-            });
+  test('getting an assertion works', function(t) {
+    request(srv)
+      .get(assertionUrl)
+      .expect(200, function(err, res) {
+        if (err) throw err;
+        t.equal(res.body.recipient.type, 'email');
+        t.equal(res.body.recipient.hashed, true);
+        t.same(res.body.verify, {
+          type: 'hosted',
+          url: assertionUrl
+        });
+        // TODO: verify salted hash is correct?
 
-            var badgeUrl = res.body.badge;
+        badgeUrl = res.body.badge;
+        t.end();
+      });
+  });
 
-            // TODO: verify salted hash is correct?
+  test('getting badge class JSON works', function(t) {
+    request(srv)
+      .get(badgeUrl)
+      .expect(200, function(err, res) {
+        if (err) throw err;
+        t.equal(res.body.name, 'Offline badge');
+        t.ok(/^data:image\/png;base64,/.test(res.body.image));
 
-            request(srv)
-              .get(badgeUrl)
-              .expect(200, function(err, res) {
-                if (err) throw err;
-                t.equal(res.body.name, 'Offline badge');
-                t.ok(/^data:image\/png;base64,/.test(res.body.image));
+        criteriaUrl = res.body.criteria;
+        issuerUrl = res.body.issuer;
+        t.end();
+      });
+  });
 
-                var criteriaUrl = res.body.criteria;
-                var issuerUrl = res.body.issuer;
+  test('getting issuer JSON works', function(t) {
+    request(srv)
+      .get(issuerUrl)
+      .expect(200, function(err, res) {
+        if (err) throw err;
+        t.same(res.body, {
+          name: 'Badge Authority',
+          org: 'Some Program',
+          contact: 'brian@example.org',
+          url: 'http://example.org/program'
+        });
+        t.end();
+      });
+  });
 
-                request(srv)
-                  .get(issuerUrl)
-                  .expect(200, function(err, res) {
-                    if (err) throw err;
-                    t.same(res.body, {
-                      name: 'Badge Authority',
-                      org: 'Some Program',
-                      contact: 'brian@example.org',
-                      url: 'http://example.org/program'
-                    });
-
-                    request(srv)
-                      .get(criteriaUrl)
-                      .expect(200, function(err, res) {
-                        t.equal(res.body.type, 'fakeRender');
-                        t.equal(res.body.template, 'public/criteria.html');
-                        srv.close();
-                        t.end();
-                      });
-                  });
-              });
-          });
-
+  test('getting criteria URL works', function(t) {
+    request(srv)
+      .get(criteriaUrl)
+      .expect(200, function(err, res) {
+        t.equal(res.body.type, 'fakeRender');
+        t.equal(res.body.template, 'public/criteria.html');
+        t.end();
       });
   });
 
   // necessary to stop the test runner
   test('shutting down #', function (t) {
+    srv.close();
     db.close();
     t.end();
   });
