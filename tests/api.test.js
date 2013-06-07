8a7493de5d17817609c76207585aa9f66e53b63f
Atul Varma <varmaa@gmail.com>
2013-06-07 09:54:48 -0400

    Tests for /v2/test/webhook also test JWT.

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,4 +1,5 @@
 const sinon = require('sinon');
+const jwt = require('jwt-simple');
 const test = require('./');
 const conmock = require('./conmock');
 const badgeFixtures = require('./badge-model.fixtures');
@@ -424,9 +425,13 @@ test.applyFixtures(badgeFixtures, function(fx) {
   });
 
   test('api can be used to test webhook success', function(t) {
-    t.plan(5);
+    t.plan(7);
     webhooks.webhookUrl = 'http://mywebhook/blah';
+    webhooks.jwtSecret = 'somekindasecret';
     sinon.stub(webhooks.request, 'post', function(options, cb) {
+      var auth = jwt.decode(options.json.auth, 'somekindasecret');
+      t.same(auth.prn, 'test@test.com');
+      t.ok(auth.exp > Date.now());
       t.same(options.url, 'http://mywebhook/blah');
       t.same(options.json.email, "test@test.com");
       t.same(options.json.claimCode, "TESTING");
