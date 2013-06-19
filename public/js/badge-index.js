91aa698acc89807241e2d460093c006f2a82bfe4
Atul Varma <varmaa@gmail.com>
2013-06-19 09:18:59 -0400

    Moved badge deletion client-side JS into common.js.
    Also made it more generic so it'll be easy to use the same code for
    deleting issuers and brograms.

--- public/js/badge-index.js
+++ public/js/badge-index.js
@@ -1,9 +1,7 @@
 (function ($) {
-  var csrfToken = $('meta[name="csrf"]').attr('content');
   var $forms = $('.js-remove-behavior');
   var $links = $forms.find('.js-show-remove');
   var $cancel = $forms.find('.js-cancel-button');
-  var $badgeDelete = $('.js-delete-badge')
 
   $links.on('click', function (e) {
     var $this = $(this);
@@ -14,21 +12,6 @@
   $cancel.on('click', function (e) {
     var $this = $(this);
     $this.parents('form').removeClass('show-actions');
-    return (e.preventDefault(), false);
-  });
-
-  $badgeDelete.on('click', function (e) {
-    var $this = $(this);
-    var row = $this.closest("tr");
-    var seriously = window.confirm('Seriously delete this badge?');
-    if (seriously)
-      $.ajax({
-        url: this.href,
-        type: 'DELETE',
-        data: {csrf: csrfToken},
-        success: function() { window.location.reload(); },
-        error: function() { alert('Alas, an error occurred.'); }
-      });
     return (e.preventDefault(), false);
   });
 })(jQuery)
--- public/js/common.js
+++ public/js/common.js
@@ -1,9 +1,25 @@
-(function logout($) {
+(function($) {
+  var csrfToken = $('meta[name="csrf"]').attr('content');
   var link = $('.js-logout-link');
   var form = $('.js-logout-form');
+
   link.on('click', function (e) {
     form.submit();
     e.preventDefault();
     return false;
+  });
+
+  $(document.body).on('click', '.js-delete-item', function(e) {
+    var $this = $(this);
+    var seriously = window.confirm('Seriously ' + this.title + '?');
+    if (seriously)
+      $.ajax({
+        url: this.href,
+        type: 'DELETE',
+        data: {csrf: csrfToken},
+        success: function() { window.location.reload(); },
+        error: function() { alert('Alas, an error occurred.'); }
+      });
+    return (e.preventDefault(), false);    
   });
 })(jQuery);
